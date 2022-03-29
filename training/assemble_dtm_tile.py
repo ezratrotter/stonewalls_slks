34e9272378b87e9dac1f61a993d6ff04385732d7
Ezra Trotter <ezratrotter@gmail.com>
2022-03-29 11:04:00 +0200

    preliminary version of cleaning data

--- training/assemble_dtm_tile.py
+++ training/assemble_dtm_tile.py
@@ -3,13 +3,38 @@ import geopandas as gpd
 import os
 import glob
 import numpy as np
+import shapely
+from osgeo import ogr
+#	1. Select 1 10km tile.
+km10 = gpd.read_file(r'D:\stonewalls_slks\data\pilot_training_data\training_10km_grid.gpkg')
 
+myTileGeom = km10.loc[0]['geometry']
+
+#	2. Buffer 10m with x meters.
+
+
+myTileGeomBuffered = myTileGeom.buffer(11.0)
+
+#	3. Clip stonewalls to (2)
+#%%
+walls = gpd.read_file('D:/stonewalls_slks/data/stonewalls/denmark')
+
+walls_clip = gpd.clip(walls, myTileGeomBuffered)
+#%%
+import matplotlib.pyplot as plt
+
+
+walls_clip.plot()
+walls.plot()
+#	4. Extract DTM raster by clipping (2) to the .vrt DTM. buteo.raster.clip
+
+#	5. Done.
+
+#%%
 km10 = gpd.read_file(r'D:\stonewalls_slks\data\pilot_training_data\training_10km_grid.gpkg')
 km1 = gpd.read_file(r'D:\stonewalls_slks\data\grids\dki_1km.gpkg')
 
 bob = np.array(km1.intersects(km10).to_list()) 
-
-import pdb; pdb.set_trace()
 
 
 # pilot_grids_gdf = gpd.read_file(r'D:\stonewalls_slks\data\pilot_training_data\training_10km_grid.gpkg')
--- training/create_profile_presence_data.py
+++ training/create_profile_presence_data.py
@@ -1,27 +1,42 @@
 
+#%%
 import geopandas as gpd
 import time
-import sys; sys.path.append('/home/ezra/stonewalls/lib')
+import sys; sys.path.append('D:/stonewalls_slks/lib')
 from datetime import datetime
+from osgeo import ogr, gdal
 
 from core import create_profiles
+#%%
+#	1. Select 1 10km tile.
+km10 = gpd.read_file(r'D:\stonewalls_slks\data\pilot_training_data\training_10km_grid.gpkg')
+walls = gpd.read_file('D:/stonewalls_slks/data/stonewalls/denmark')
+myTileGeom = km10.loc[0]['geometry']
 
-start = time.time() 
+#%%
+#	2. Buffer 10m with x meters.
+myTileGeomBuffered = myTileGeom.buffer(11.0)
+walls_clip = gpd.clip(walls, myTileGeomBuffered)
 
-path_to_stonewalls = '/mnt/c/Users/ezra/OneDrive - NIRAS/thesis/Data/aeroe/Stonewalls/Stonewalls_AEROE.shp'
-dtm = '/mnt/c/Users/ezra/Desktop/AEROE_terainn_data/DTM/DTM_AEROE.vrt'
-output_folder = '/mnt/c/Users/ezra/Desktop/wall_output/'
-distance = 0.4
+#%%
+#	3. Clip stonewalls to (2)
+
+#   4. Extract DTM raster by clipping (2) to the .vrt DTM. buteo.raster.clip
+vrt = 'M:/Ekstern_datasamling/Danmark/Frie_DATA/DTM_2019/DTM_Grid_1km_TIFF/_merged.vrt'
+
+
+
+output_folder = 'D:/OUT'
+distance = 10
 date = datetime.now().strftime("%m-%d-%Y")
 
 
-gdf = gpd.read_file(path_to_stonewalls)
-gdf = gdf.to_crs(epsg=25832)
+gdf = walls_clip.to_crs(epsg=25832)
 
 # activate and replace first arg of create_profiles for smaller subset
-sub_gdf = gdf[:50]
+# sub_gdf = gdf[:50]
 
-profiles, walls = create_profiles(sub_gdf, dtm, subwall_distance=distance)
+profiles, walls = create_profiles(gdf, vrt, subwall_distance=distance)
 
 profiles_out = '{}profiles_{}_{}.geojson'.format(output_folder, str(distance)+'m', date)
 walls_out = '{}walls_{}_{}.geojson'.format(output_folder, str(distance)+'m', date)
@@ -34,5 +49,7 @@ finish = time.time()
 print("Time Taken is {0}s".format(finish - start))
 print(output_folder)
 
+#%%
 
 
+# %%
