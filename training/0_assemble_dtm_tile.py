c4d3d15ab4ee863abf944210e01f9f2e662a8f84
Ezra Trotter <ezratrotter@gmail.com>
2022-03-31 11:53:02 +0200

    training area move tiles

--- training/0_assemble_dtm_tile.py
+++ training/0_assemble_dtm_tile.py
@@ -18,7 +18,6 @@ myTileGeomBuffered = myTileGeom.buffer(11.0)
 #	3. Clip stonewalls to (2)
 walls_clip = gpd.clip(walls, myTileGeomBuffered)
 #%%
-wrap_command = 'gdalwarp -of GTiff -cutline D:/VIIRS/gha_admbndp0_1m_gaul/GHA_admbndp0_1m_GAUL.shp -cl GHA_admbndp0_1m_GAUL -crop_to_cutline {input1} {output1}'
 
 #   4. Extract DTM raster by clipping (2) to the .vrt DTM. buteo.raster.clip
 vrt = 'M:/Ekstern_datasamling/Danmark/Frie_DATA/DTM_2019/DTM_Grid_1km_TIFF/_merged.vrt'
@@ -34,37 +33,39 @@ vrt = 'M:/Ekstern_datasamling/Danmark/Frie_DATA/DTM_2019/DTM_Grid_1km_TIFF/_merg
 
 
 #%%
-# pilot_grids_gdf = gpd.read_file(r'D:\stonewalls_slks\data\pilot_training_data\training_10km_grid.gpkg')
-# # grids_list = pilot_grids_gdf['KN10kmDK'].tolist()
+import geopandas as gpd
+import os
+import glob
+import numpy as np
+import shapely
+from osgeo import ogr, gdal
+
+km10training = gpd.read_file(r'D:\stonewalls_slks\data\pilot_training_data\training_10km_grid.gpkg')
+
+km1 = gpd.read_file(r'D:\stonewalls_slks\data\grids\dki_1km.gpkg')
+km10 = gpd.read_file(r'D:\stonewalls_slks\data\grids\dki_10km.gpkg')
 
 
-# all_1km_grids_gdf = gpd.read_file(r'D:\stonewalls_slks\data\grids\dki_1km.gpkg')
+km10training['geometry'] = km10training.apply(lambda x: x['geometry'].buffer(1), axis=1)
 
-
-# our_1km_grids_gdf = all_1km_grids_gdf[all_1km_grids_gdf['dki_10km'].isin(grids_list)]
-
-# # %%
-# pilot_grids_list = our_1km_grids_gdf['dki_1km'].tolist()
-# pilot_files_list = ['DTM_' + x for x in pilot_grids_list]
-# dir_to_tifs = r'M:\Ekstern_datasamling\Danmark\Frie_DATA\DTM_2019\DTM_Grid_1km_TIFF' 
-
-# list_of_tifs = glob.glob(os.path.join(dir_to_tifs, '*.tif'))
-# # %%
-# smaller_list = [x for x in list_of_tifs if os.path.basename(os.path.splitext(x)[0]) in pilot_files_list]
-# # os.path.basename(os.path.splitext(path)[0])
-# #%%
-# # if move to local
-
-# local_folder = 'D:/tifs'
-# os.path.isdir(local_folder)
-
-# [shutil.copy(x, local_folder + '/' + pilot_files_list[i]) for x in enumerate(smaller_list)]
+tile_list = gpd.overlay(km1, km10training, how='intersection')['dki_1km'].tolist()
+print(len(tile_list), 'tiles....')
 
 #%%
+import shutil
+import os
+DTM_source_dir = 'M:/Ekstern_datasamling/Danmark/Frie_DATA/DTM_2019/DTM_Grid_1km_TIFF/'
+DSM_source_dir = 'M:/Ekstern_datasamling/Danmark/Frie_DATA/DTM_2019/DSM_Grid_1km_TIFF/'
+
+DTM_destination_dir = 'V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dtm/'
+DSM_destination_dir = 'V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dsm/'
 
 
-# from osgeo import gdal 
-
-# vrt_options = gdal.BuildVRTOptions(resampleAlg='cubic', addAlpha=True)
-# my_vrt = gdal.BuildVRT('my.vrt', smaller_list)
-# my_vrt = None
+print('moving files...', 'count: ', len(tile_list))
+for tile in tile_list:
+    dtm_source_tif = os.path.join(DTM_source_dir, 'DTM_'+ tile +'.tif' )
+    dsm_source_tif = os.path.join(DSM_source_dir, 'DSM_'+ tile +'.tif' )
+    # destination = destination_dir + tile_name
+    shutil.copy(dtm_source_tif, DTM_destination_dir)
+    shutil.copy(dsm_source_tif, DSM_destination_dir)
+    print('moving: ',tile, '...')
