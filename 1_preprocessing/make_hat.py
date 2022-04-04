b7e4e4f6eacaac0cada9d7f0bc9881261a76c92e
Ezra Trotter <ezratrotter@gmail.com>
2022-04-04 15:35:23 +0200

    updates

--- 1_preprocessing/make_hat.py
+++ 1_preprocessing/make_hat.py
@@ -1,4 +1,3 @@
-
 import subprocess
 import glob
 import os
@@ -9,35 +8,36 @@ DSM_1km_6052_660.tif
 DTM_1km_6049_685.tif
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    dtm_dir = 'V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dtm/'
-    dsm_dir = 'V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dsm/'
-    hat_dir = 'V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/hat/'
+    dtm_dir = "V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dtm/"
+    dsm_dir = "V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dsm/"
+    hat_dir = "V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/hat/"
 
-    dtm_list = glob.glob(r'V:\2022-03-31_Stendiger_EZRA\training_data\initial_area\dem\dtm\*.tif')
-    dsm_list = glob.glob(r'V:\2022-03-31_Stendiger_EZRA\training_data\initial_area\dem\dsm\*.tif')
+    dtm_list = glob.glob(
+        r"V:\2022-03-31_Stendiger_EZRA\training_data\initial_area\dem\dtm\*.tif"
+    )
+    dsm_list = glob.glob(
+        r"V:\2022-03-31_Stendiger_EZRA\training_data\initial_area\dem\dsm\*.tif"
+    )
 
     if len(dtm_list) != len(dsm_list):
         print(len(dtm_list), len(dsm_list))
-        print('DTM and DSM files do not have the same number of files')
-    
+        print("DTM and DSM files do not have the same number of files")
 
-    tile_list = [x.split('\\')[-1] for x in dtm_list]
+    tile_list = [x.split("\\")[-1] for x in dtm_list]
     count = 0
     for t in tile_list:
 
         dtm = dtm_dir + t
-        dsm = dsm_dir + t.replace('DTM', 'DSM')
-        hat_file = hat_dir + t.replace('DTM', 'HAT')
+        dsm = dsm_dir + t.replace("DTM", "DSM")
+        hat_file = hat_dir + t.replace("DTM", "HAT")
         if os.path.exists(hat_file):
-            print('HAT file already exists: ', hat_file)
             continue
         try:
-            calccommand ="gdal_calc -A {dsm} -B {dtm} --outfile={hat_file} --NoDataValue=-9999 --type=Float32 --calc=A-B".format(dtm=dtm, dsm=dsm, hat_file=hat_file)
+            calccommand = f'gdal_calc -A {dsm} -B {dtm} --outfile={hat_file} --co="COMPRESS=LZW" --NoDataValue=-9999 --type=Float32 --calc=A-B'
             subprocess.call(calccommand, shell=True)
-            print('HAT file created: ', hat_file)
+            print("HAT file created: ", hat_file)
         except:
-             print('FAILED: ', hat_file)           
-    print('checked')
-
+            print("FAILED: ", hat_file)
+    print("checked")
--- 1_preprocessing/validate_transfer.py
+++ 1_preprocessing/validate_transfer.py
@@ -7,9 +7,11 @@ import shapely
 from osgeo import ogr, gdal
 import shutil
 
-def transfer_tiles(tile_type, source_dir, dest_dir, tile_list):
-    if tile_type != 'dtm' or tile_type != 'dsm':
-        raise ValueError('tile_type must be either dtm or dsm')
+def validate_tile_transfer(tile_type, source_dir, dest_dir, tile_list):
+
+    if tile_type != 'DTM' and tile_type != 'DSM':
+        print('tile_type: ', tile_type)
+        raise ValueError('tile_type must be either DTM or DSM')
     if not os.path.exists(source_dir):
         raise ValueError('source_dir doesnt exist')
     if not os.path.exists(dest_dir):
@@ -19,16 +21,16 @@ def transfer_tiles(tile_type, source_dir, dest_dir, tile_list):
     if len(tile_list) == 0:
         raise ValueError('tile_list must not be empty')
 
-        
-    print('moving files...', 'count: ', len(tile_list))
+    print('validating files...', 'count: ', len(tile_list))
     for tile in tile_list:
         
-        source_tif = os.path.join(tile_type, source_dir, tile_type + '_' + tile +'.tif' )
-        dest_tif = os.path.join(tile_type, dest_dir, tile_type + '_' + tile +'.tif' )
+        source_tif = os.path.join(DTM_source_dir, tile_type + '_' + tile +'.tif' )
+        dest_tif = os.path.join(DTM_destination_dir, tile_type + '_' + tile +'.tif' )
 
-        if not os.path.exists(dtm_dest_tif):
-            shutil.copy(source_tif, dest_tif)
+        if not os.path.exists(source_tif):
             print('file didnt exist: ', source_tif)
+        if not os.path.exists(dest_tif):
+            print('file did not transfer: ', dest_tif)
         
     print('finished')
 
@@ -37,7 +39,7 @@ if __name__ == '__main__':
     km10training = gpd.read_file(r'V:\2022-03-31_Stendiger_EZRA\stonewalls_slks\data\pilot_training_data\training_10km_grid.gpkg')
     km10training['geometry'] = km10training.apply(lambda x: x['geometry'].buffer(1), axis=1)
     km1 = gpd.read_file(r'V:\2022-03-31_Stendiger_EZRA\stonewalls_slks\data\grids\dki_1km.gpkg')
-    tile_list = gpd.overlay(km1, km10training, how='intersection')['dki_1km'].tolist()
+    tile_list = gpd.overlay(km1, km10training, how='intersection')['dki_1km'].unique().tolist()
 
 
 
@@ -47,5 +49,5 @@ if __name__ == '__main__':
     DTM_destination_dir = 'V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dtm/'
     DSM_destination_dir = 'V:/2022-03-31_Stendiger_EZRA/training_data/initial_area/dem/dsm/'
 
-    transfer_tiles(DTM_source_dir, DTM_destination_dir, tile_list)
-    transfer_tiles(DSM_source_dir, DSM_destination_dir, tile_list)
+    validate_tile_transfer('DTM', DTM_source_dir, DTM_destination_dir, tile_list)
+    validate_tile_transfer('DSM', DSM_source_dir, DSM_destination_dir, tile_list)
